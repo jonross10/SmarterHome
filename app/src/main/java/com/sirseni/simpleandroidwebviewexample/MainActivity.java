5342202070004080712b1677b8cafbc88cb0b9d0
Jon Ross <jonathan.ross15@gmail.com>
2017-01-27 15:36:00 -0500

    Pushing Project

--- app/src/main/java/com/sirseni/simpleandroidwebviewexample/MainActivity.java
+++ app/src/main/java/com/sirseni/simpleandroidwebviewexample/MainActivity.java
@@ -1,32 +1,168 @@
 package com.sirseni.simpleandroidwebviewexample;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.telephony.TelephonyManager;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import com.android.volley.ApplicationController;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Timer;
+
 public class MainActivity extends Activity {
 
+    private boolean isPageLoadedComplete = false; //declare at class level
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    public MainActivity() throws JSONException {
+    }
+
+
+    @SuppressLint("HardwareIds")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView myWebView = (WebView) findViewById(R.id.myWebView);
-        myWebView.loadUrl("http://www.centerend.com");
+        final WebView myWebView = (WebView) findViewById(R.id.myWebView);
+
+        new CountDownTimer(3000, 1000) {
+
+            @Override
+            public void onTick(long l) {
+                System.out.println("tick");
+            }
+
+            public void onFinish() {
+                if (isPageLoadedComplete) {
+                } else {
+                    myWebView.loadUrl("file:///android_asset/errorpage.html");
+                    setContentView(myWebView);
+                }
+            }
+        }.start();
+
+        Timer myTimer = new Timer();
+        //Start this timer when you create you task
+        myWebView.loadUrl("http://192.168.1.120");
         myWebView.setWebViewClient(new MyWebViewClient());
         WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
+        myWebView.setWebChromeClient(new WebChromeClient());
+        myWebView.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                view.loadUrl("file:///android_asset/errorpage.html");
+                setContentView(view);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                isPageLoadedComplete = true;
+                TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+                String phone = tm.getLine1Number();
+                myWebView.loadUrl("javascript: localStorage.setItem(\"phonenumber\", " + phone + ");");
+            }
+        });
+
+        final String URL = "http://192.168.1.120:8123/api/services/light/toggle";
+        // Post params to be sent to the server
+        HashMap<String, String> param = new HashMap<String, String>();
+        param.put("entity_id","light.hue_white_lamp_1");
+
+        JsonObjectRequest req = new JsonObjectRequest(URL, new JSONObject(param),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            VolleyLog.v("Response:%n %s", response.toString(4));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                VolleyLog.e("Error: ", error.getMessage());
+            }
+        });
+
+        ApplicationController.getInstance().addToRequestQueue(req);
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+
 
     // Use When the user clicks a link from a web page in your WebView
     private class MyWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            if (Uri.parse(url).getHost().equals("www.centerend.com")) {
+            if (Uri.parse(url).getHost().equals("192.168.1.120")) {
                 return false;
 
             }
