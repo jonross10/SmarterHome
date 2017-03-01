ed24f74e30e2cad2fa599f2b9ed555fdbdd10bd6
jonross10 <jonathan.ross15@gmail.com>
2017-03-01 16:26:19 -0500

    Completed beacon sensing and sending json array to notify.php. App now functions properly

--- app/src/main/java/com/sirseni/simpleandroidwebviewexample/MainActivity.java
+++ app/src/main/java/com/sirseni/simpleandroidwebviewexample/MainActivity.java
@@ -7,7 +7,9 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.RemoteException;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -23,13 +25,27 @@ import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
 
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Timer;
+import org.json.JSONObject;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements BeaconConsumer {
+
+    protected static final String TAG = "RangingActivity";
+    private BeaconManager beaconManager;
+
+    String phone;
 
     private boolean isPageLoadedComplete = false; //declare at class level
     /**
@@ -47,6 +63,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+
+        beaconManager.getBeaconParsers().add(new BeaconParser().
+                setBeaconLayout(BeaconParser.EDDYSTONE_UID_LAYOUT));
+        beaconManager.bind(this);
+
 
         final WebView myWebView = (WebView) findViewById(R.id.myWebView);
 
@@ -85,40 +108,83 @@ public class MainActivity extends Activity {
             public void onPageFinished(WebView view, String url) {
                 isPageLoadedComplete = true;
                 TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-                String phone = tm.getLine1Number();
+                phone = tm.getLine1Number();
                 myWebView.loadUrl("javascript: localStorage.setItem(\"phonenumber\", " + phone + ");");
             }
         });
 
-        final String URL = "http://192.168.1.120:8123/api/services/light/toggle";
-        // Post params to be sent to the server
-        HashMap<String, String> param = new HashMap<String, String>();
-        param.put("entity_id","light.hue_white_lamp_1");
-
-        JsonObjectRequest req = new JsonObjectRequest(URL, new JSONObject(param),
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            VolleyLog.v("Response:%n %s", response.toString(4));
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                VolleyLog.e("Error: ", error.getMessage());
-            }
-        });
-
-        ApplicationController.getInstance().addToRequestQueue(req);
 
         // ATTENTION: This was auto-generated to implement the App Indexing API.
         // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.addRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    Log.i(TAG, "The first beacon I see is about "+beacons.iterator().next().getDistance()+" meters away.");
+                    Log.i(TAG, "RSSI: "+beacons.iterator().next().getRssi());
+                        final String URL = "http://192.168.1.120/php/notify.php";
+                        // Post params to be sent to the server
+                    try {
+                        JSONArray jsonArr = new JSONArray();
+                        for (Beacon beacon : beacons) {
+                            JSONObject JSONbeacons = new JSONObject();
+                            JSONbeacons.put("beaconID", Integer.parseInt(beacon.getId2().toString().substring(2), 16));
+                            JSONbeacons.put("rssi", beacon.getRssi());
+                            jsonArr.put(JSONbeacons);
+                        }
+                        JSONObject rangesJSON = new JSONObject();
+
+                        rangesJSON.put("beacons", jsonArr);
+                        String ranges = rangesJSON.toString();
+                        Log.i(TAG, ranges);
+
+
+                        Log.i(TAG, ranges);
+                        JSONObject jsonSend = new JSONObject();
+                        jsonSend.put("phoneNumber",phone);
+                        jsonSend.put("ranges", rangesJSON);
+                        JsonObjectRequest req = new JsonObjectRequest(URL, jsonSend,
+                                new Response.Listener<JSONObject>() {
+                                    @Override
+                                    public void onResponse(JSONObject response) {
+                                        try {
+                                            VolleyLog.v("TAG", response.toString(4));
+                                            Log.i(TAG, "RESPONSE\n");
+                                            Log.i(TAG,response.toString(4));
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }, new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                VolleyLog.e("Error: ", error.getMessage());
+                            }
+                        });
+
+                        ApplicationController.getInstance().addToRequestQueue(req);
+                    } catch(JSONException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        try {
+            beaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+        } catch (RemoteException e) {    }
+    }
 
 
     /**
